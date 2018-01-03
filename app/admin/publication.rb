487896778d0c14b1d75dbeebd99ac1fea288da00
Faizan Ah <faizuali4@gmail.com>
2018-01-03 17:59:24 +0500

    set active admin publication model

--- app/admin/publication.rb
+++ app/admin/publication.rb
@@ -1,15 +1,59 @@
 ActiveAdmin.register Publication do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  actions :all
+
+  menu priority: 100
+  permit_params :title , :description , :publisher , :publication_date , :publication_url
+  reorderable
+  before_create do |publication|
+    publication.admin_user_id = current_admin_user.id
+  end
+
+  controller do
+    def scoped_collection
+      super.where(admin_user_id: current_admin_user.id)
+    end
+
+    def find_resource
+      scoped_collection.where(id: params[:id]).first!
+    end
+  end
+
+  config.filters = false
+
+  index  as: :reorderable_table ,download_links: false do |org|
+    column  :title do |publication|
+      best_in_place publication, :title, url: [:admin, publication]
+    end
+    column  :publisher do |publication|
+      best_in_place publication, :publisher, url: [:admin, publication]
+    end
+    column :publication_date
+    actions
+  end
+
+  show do
+    attributes_table do
+      row  :title do |publication|
+        best_in_place publication, :title, url: [:admin, publication]
+      end
+      row :publisher
+      row :publication_date
+      row :publication_url
+      row :description
+      row :created_at
+      row :updated_at
+    end
+  end
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs "Publication Details" do
+      f.input :title , label: "Title"
+      f.input :publisher
+      f.input :publication_date
+      f.input :publication_url
+      f.input :description , as: :text
+    end
+    f.actions
+  end
 
 end
