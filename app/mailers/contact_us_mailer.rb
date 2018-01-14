f96dda4354101e19597604b25012b8665bfe5c35
Faizan Ahmad <faizu_ali@yahoo.com>
2018-01-15 01:01:38 +0500

    smtp fixes

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -3,7 +3,7 @@ class ContactUsMailer < ApplicationMailer
   def send_email(contact)
     @contact = contact
     @user = contact.admin_user
-    mail( to: @user.profile.email, subject: @contact.subject , from:  @contact.email)
+    mail( to: @user.profile.email, subject: @contact.subject , from:  @user.profile.email)
   end
 
   def thank_you_for_contact_us contact
