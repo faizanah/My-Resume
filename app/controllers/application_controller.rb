c5370932bd97bfd3d5b4fa8abcd4b4db64c484e3
Faizan Ah <faizuali4@gmail.com>
2018-03-15 12:04:48 +0500

    check visiter location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
   helper_method :current_profile , :current_user
-  layout :set_layout
+  # layout :set_layout
   before_action :check_visiter
 
   def current_profile
@@ -15,18 +15,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_layout
-    current_profile.layout_name
-  end
+  # def set_layout
+  #   current_profile.layout_name
+  # end
 
   def check_visiter
     puts "Location: "
-    # puts "IP: #{request.ip}"
-    # puts "country_code: #{request.country_code}"
-    # puts "country_name: #{request.country_name}"
-    # puts "region_code: #{request.region_code}"
-    # puts "region_name: #{request.region_name}"
-    # puts request.location
+    puts "IP: #{request.ip}"
+    puts "country_code: #{request.country_code}"
+    puts "country_name: #{request.country_name}"
+    puts "region_code: #{request.region_code}"
+    puts "region_name: #{request.region_name}"
+    puts request.location
   end
 
 end
