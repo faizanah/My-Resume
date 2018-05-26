8e923ff593eae1ad63db6afbd92ab50e9d7c9fcb
Faizan Ahmad <faizu_ali@yahoo.com>
2018-05-26 14:11:47 +0500

    setup sendgrid for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,16 +28,27 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => ENV['domain'],
-      :user_name            => ENV['gmail_username'],
-      :password             => ENV['gmail_password'],
-      :authentication       => "plain",
-      :enable_starttls_auto => true
+  
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],,
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
+  
+  # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #     :address              => "smtp.gmail.com",
+  #     :port                 => 587,
+  #     :domain               => ENV['domain'],
+  #     :user_name            => ENV['gmail_username'],
+  #     :password             => ENV['gmail_password'],
+  #     :authentication       => "plain",
+  #     :enable_starttls_auto => true
+  # }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
